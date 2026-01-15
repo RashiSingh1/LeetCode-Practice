ee7d64f297012c70273092098e29d0f32190ee0a
Rashi Kumari <01rashi.kumari@gmail.com>
2026-01-15 17:54:26 +0000

    solve: Runtime - 0 ms (100.00%), Memory - 43.6 MB (43.47%)

--- 0027-remove-element/solution.java
+++ 0027-remove-element/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; 
+        int k = 0;  // count of elements not equal to val
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
