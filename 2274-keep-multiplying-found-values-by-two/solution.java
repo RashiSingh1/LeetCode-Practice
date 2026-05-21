b7e789653de77a2b959abba8d8494bd374901da5
Rashi Kumari <01rashi.kumari@gmail.com>
2026-05-21 16:49:54 +0000

    solve: Runtime - 7 ms (14.50%), Memory - 46.4 MB (38.20%)

---
+++ 2274-keep-multiplying-found-values-by-two/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==original){
+                original = 2*original;
+            }
+        }
+        return original;
+    }
+}
