67d0a75ae300920ce880d1b5be2ddc3ffadcbef3
Rashi Kumari <01rashi.kumari@gmail.com>
2026-05-21 17:20:26 +0000

    solve: Runtime - 1 ms (83.51%), Memory - 43.1 MB (60.21%)

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        for(int i =0; i<=nums.length; i++){
+            int count = 0;
+            for(int ele : nums){
+                if(ele>= i)count++;
+            }
+            if(i==count) return i;
+        }
+        return -1;
+        
+    }
+}
