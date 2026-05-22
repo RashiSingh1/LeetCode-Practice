c28419b034dcc94e1d5274c8c501818b6ce8ec0f
Rashi Kumari <01rashi.kumari@gmail.com>
2026-05-22 09:09:51 +0000

    solve: Runtime - 5 ms (95.91%), Memory - 46.6 MB (42.70%)

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int n= seats.length , sum =0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        for(int i=0;i<n;i++){
+            sum+= Math.abs(seats[i]-students[i]);
+        }
+        return sum;
+    }
+}
