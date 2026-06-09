b868c6c1db618b81355f1df16144df0f5d35f2ba
Rashi Kumari <01rashi.kumari@gmail.com>
2026-06-09 09:24:02 +0000

    solve: Runtime - 1 ms (100.00%), Memory - 62 MB (85.86%)

---
+++ 4005-maximum-total-subarray-value-i/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int min = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return 1L * (max - min) * k;
+    }
+}
