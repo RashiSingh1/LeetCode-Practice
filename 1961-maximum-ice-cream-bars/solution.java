068bba28b5b9aa03d4bf0cad68d54d88184b258a
Rashi Kumari <01rashi.kumari@gmail.com>
2026-06-21 11:25:25 +0000

    solve: Runtime - 38 ms (17.87%), Memory - 83.8 MB (51.26%)

--- 1961-maximum-ice-cream-bars/solution.java
+++ 1961-maximum-ice-cream-bars/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
        Arrays.sort(costs);
-       int sum =0;
-       int count =0;
-       for(int cost : costs){
-        if(sum+cost<=coins){
-            sum = sum+cost;
-            count++;
+       for(int i=0;i<costs.length;i++){
+        if(coins>=costs[i]){
+            coins = coins-costs[i];
         }
-       }
-       return count;
+        else{
+            return i;
+        }
+       } 
+       return costs.length;
     }
 }
