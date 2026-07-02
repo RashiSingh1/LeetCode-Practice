ea2f043cc94cc4a2fbb1cc3f7f97a17115813ea3
Rashi Kumari <01rashi.kumari@gmail.com>
2026-07-02 12:19:21 +0000

    solve: Runtime - 6 ms (98.24%), Memory - 143.1 MB (41.91%)

---
+++ 0624-maximum-distance-in-arrays/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int min = arrays.get(0).get(0);
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        int ans = 0;
+        for (int i = 1; i < arrays.size(); i++) {
+            List<Integer> current = arrays.get(i);
+            int currMin = current.get(0);
+            int currMax = current.get(current.size() - 1);
+            ans = Math.max(ans, Math.abs(currMax - min));
+            ans = Math.max(ans, Math.abs(max - currMin));
+            min = Math.min(min, currMin);
+            max = Math.max(max, currMax);
+        }
+        return ans;
+    }
+}
