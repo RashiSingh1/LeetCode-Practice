3a8b4890f210d7c85b45abe58b0297beeda6d72d
Rashi Kumari <01rashi.kumari@gmail.com>
2026-07-07 23:14:43 +0530

    Time: 9 ms (53.78%), Space: 47.9 MB (72.38%) - LeetHub

---
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        int res = 0;
+        int[] ageFreq = new int[121];
+        for (int age : ages) {
+            ageFreq[age]++;
+        }
+        for (int age : ages) {
+            int minValidAge = ((int)(0.5 * age) + 7) + 1;
+            int maxValidAge = age;
+            for (int j = minValidAge; j <= maxValidAge; j++) {
+                res+= ageFreq[j];
+                if (j == age) {
+                    res--;
+                }
+            }
+        }
+        return res;
+    }
+}
