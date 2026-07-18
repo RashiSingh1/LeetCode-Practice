74cf9be4ec88e73abf5933b315da2c7b4c07bbb3
Rashi Kumari <01rashi.kumari@gmail.com>
2026-07-18 10:00:43 +0530

    Time: 4 ms (47.76%), Space: 44.9 MB (42.15%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        String original = sb.toString();
+        String reversed = sb.reverse().toString();
+        return original.equals(reversed);
+    }
+}
