9dad6097270aba41a55fd51d51e75bb7883d2618
Rashi Kumari <01rashi.kumari@gmail.com>
2026-08-10 19:49:14 +0530

    Time: 16 ms (59.53%), Space: 42.4 MB (83.55%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean winnerSquareGame(int n) {
         boolean[] isWinningState = new boolean[n + 1];
         for (int i = 1; i <= n; i++) {
-            for (int k = 1; k * k <= i; k++) {
-                if (!isWinningState[i - k * k]) {
+            for (int j = 1; j * j <= i; j++) {
+                if (!isWinningState[i - j * j]) {
                     isWinningState[i] = true;
                     break; 
                 }
