f4777fcd07d9ea65a9afd4fade0e868a206e72b8
Rashi Kumari <01rashi.kumari@gmail.com>
2026-09-09 08:55:49 +0000

    solve: Runtime - 2 ms (100.00%), Memory - 100 MB (61.85%)

---
+++ 4382-count-robot-groups/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countGroups(int[] position, int[] speed, int distance) {
+        int n = position.length;
+        int groups = 1;
+        int groupSpeed = speed[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (position[i + 1] - position[i] <= distance) {
+                continue;
+            }
+            if (speed[i] <= groupSpeed) {
+                groups++;
+                groupSpeed = speed[i];
+            }
+        }
+        return groups;
+    }
+}
