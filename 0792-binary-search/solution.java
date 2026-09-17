3fd9c800f65767266b149305aaf90d53383fb459
Rashi Kumari <01rashi.kumari@gmail.com>
2026-09-17 04:59:19 +0000

    solve: Runtime - 0 ms (100.00%), Memory - 48.2 MB (69.33%)

--- 0792-binary-search/solution.java
+++ 0792-binary-search/solution.java
@@ -1,19 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n= nums.length;
-        int left =0, right = n-1;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
+        
+        int n = nums.length;
+        int start = 0, end = n - 1;
+
+        while (start <= end) {
+            
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
             }
-            else if(nums[mid]<target){
-                left = mid+1;
+            
+            if (nums[mid] < target) {
+                start = mid + 1;
             }
-            else{
-                right = mid-1;
+            
+            else {
+                end = mid - 1;
             }
         }
+
         return -1;
     }
 }
